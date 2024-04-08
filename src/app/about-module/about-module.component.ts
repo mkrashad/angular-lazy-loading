a4e1d9f497409a7978cb1d29ac68011a6ee95e27
Rashad Mahmudov <mkrashad25@gmail.com>
2024-04-08 19:25:53 +0200

    Create communication layer the application and the lazy loaded module

--- src/app/about-module/about-module.component.ts
+++ src/app/about-module/about-module.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommunicationService } from '../communication.service';
 
 @Component({
   selector: 'app-about-module',
   templateUrl: './about-module.component.html',
   styleUrls: ['./about-module.component.scss'],
 })
-export class AboutModuleComponent {
-  constructor() {
+export class AboutModuleComponent implements OnInit  {
+  message: string = '';
+
+  constructor(private communicationService: CommunicationService) {
     console.log('lazy loading');
+  }
+
+  ngOnInit(): void {
+    this.communicationService.currentMessage.subscribe(message => this.message = message);
+  }
+
+  sendMessage(): void {
+    this.communicationService.changeMessage('Hello from About component!');
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { CommunicationService } from './communication.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-lazy-loading';
+  message: string = '';
+
+  constructor(private communicationService: CommunicationService) {}
+
+  ngOnInit(): void {
+    this.communicationService.currentMessage.subscribe(
+      (message) => (this.message = message)
+    );
+  }
 }
